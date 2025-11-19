b35e7e824aa2e8caa76285bfd246df8e7cde2850
Stanley Sujith Nelavala <stanleysujithnelavala@gmail.com>
2025-11-19 13:07:37 -0600

    Initialize Terraform configuration for AWS S3 bucket

---
+++ terraform/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "demo" {
+  bucket = "stanley-hello-world-demo-bucket"
+}
